d5c932122283c47b3a35812cc4e4c87d4dd08ee6
Ahmad Fraidoon Faqiri <ahmad.faqiri@twido.fr>
2020-04-30 09:16:25 +1000

    cross account s3 access

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,7 @@
 import core = require('@aws-cdk/core');
 import s3 = require('@aws-cdk/aws-s3');
 import { BlockPublicAccess } from '@aws-cdk/aws-s3';
+import { AccountPrincipal } from '@aws-cdk/aws-iam';
 
 export class HelloCdkStack extends core.Stack {
   constructor(scope: core.Construct, id: string, props?: core.StackProps) {
@@ -11,17 +12,15 @@ export class HelloCdkStack extends core.Stack {
     // This means its initialization signature has scope, id, and props and it is a child of the stack.
     // FirstCdkBucket is the logical id of the bucket constuct, not the physical bucket name
     // Set the bucketName property when you define your bucket for the physical bucket name
-    new s3.Bucket(this, 'FirstCdkBucket', {
-      versioned: true,
-      encryption: s3.BucketEncryption.KMS_MANAGED
-    })
-
-    new s3.Bucket(this, 'FirstCdkBucket2', {
-      bucketName: "affaqiri-test3",
+    const bucket = new s3.Bucket(this, 'FirstCdkBucket', {
+      bucketName: "affaqiri-test5",
       versioned: true,
       publicReadAccess: false,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.KMS_MANAGED
     })
+
+    bucket.grantReadWrite(new AccountPrincipal('766627780664'));
+    //bucket.grantReadWrite(new AccountPrincipal()
   }
 }
